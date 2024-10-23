cb577d0cdd3516846276e62213b8e9b27ab08e8a
Kirsten Bachert <kb@buildmidwestern.com>
2024-10-23 09:01:04 -0600

    [fix] prettied up the null state styles + behavior

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,39 +2,37 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
+import PlaceholderImage from "../assets/images/placeholder.jpg";
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isLoading } = useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
   }
 
-  if (!isAuthenticated) {
-    return null;
-  }
-
   return (
     <div>
-      {user && (
-        <div className="bg-gray-100 p-4 rounded-lg shadow-md">
-          <div className="flex">
-            <img
-              src={user.picture}
-              alt={user.name}
-              className="rounded-full h-20 w-20"
-            />
-            <div className="flex flex-col ml-4">
-              <h2 className="text-xl font-semibold">{user.name}</h2>
-              <p className="text-gray-600">{user.email}</p>
-              <div className="flex">
-                <LoginButton />
-                <LogoutButton />
-              </div>
+      <div className="bg-gray-100 p-4 rounded-lg shadow-md">
+        <div className="flex">
+          <img
+            src={user ? user.picture : PlaceholderImage}
+            alt={user ? user.name : "placeholder image"}
+            className="rounded-full h-20 w-20"
+          />
+          <div className="flex flex-col ml-4">
+            <h2 className="text-xl font-semibold">
+              {user ? user.name : "Hello"}
+            </h2>
+            <p className="text-gray-600">
+              {user ? user.email : "Please login below:"}
+            </p>
+            <div className="flex">
+              {user ? <LogoutButton /> : <LoginButton />}
             </div>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
