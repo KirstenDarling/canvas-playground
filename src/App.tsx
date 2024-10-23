782756d7a522091a305491c995bebd4e6fb5e3ff
Kirsten Bachert <kb@buildmidwestern.com>
2024-10-23 08:44:28 -0600

    [fix] moved login/logout buttons into user profile area

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
 import Canvas from "./components/Canvas";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -9,8 +7,6 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div className="flex flex-col m-5 justify-center">
-        <LoginButton />
-        <LogoutButton />
         <Profile />
         <Routes>
           <Route path="/Canvas" element={<Canvas />} />{" "}
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -9,7 +11,7 @@ const Profile = () => {
   }
 
   if (!isAuthenticated) {
-    return null; // Or you can return a message like "You are not logged in."
+    return null;
   }
 
   return (
@@ -25,6 +27,10 @@ const Profile = () => {
             <div className="flex flex-col ml-4">
               <h2 className="text-xl font-semibold">{user.name}</h2>
               <p className="text-gray-600">{user.email}</p>
+              <div className="flex">
+                <LoginButton />
+                <LogoutButton />
+              </div>
             </div>
           </div>
         </div>
