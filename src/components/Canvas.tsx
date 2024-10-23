17e7cb0909084ebf17501968dae5d34bbac2c818
Kirsten Bachert <kb@buildmidwestern.com>
2024-10-23 09:28:21 -0600

    [fix] only show canvas if authenticated

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import ImageCanvas from "./ImageCanvas";
 import ImageGallery from "./ImageGallery";
 import PhotoBookModal from "./PhotoBookModal";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Canvas: React.FC = () => {
   const [images, setImages] = useState<any[]>([]);
@@ -29,30 +30,37 @@ const Canvas: React.FC = () => {
     console.log("Using images:", images);
     setIsPhotoBookModalOpen(false);
   };
+
+  const { isAuthenticated, isLoading } = useAuth0();
+
   return (
-    <div>
-      <PhotoBookModal
-        isOpen={isPhotoBookModalOpen}
-        onClose={() => setIsPhotoBookModalOpen(false)}
-        onCreate={handleCreatePhotoBook}
-        options={photoBookOptions}
-        onChange={handlePhotoBookChange}
-      />
-      <ImageGallery
-        onDrop={handleDrop}
-        images={images}
-        isVisible={isGalleryVisible}
-      />
-      <ImageCanvas
-        images={images}
-        isGalleryVisible={isGalleryVisible}
-        setIsGalleryVisible={setIsGalleryVisible}
-        isPhotoBookModalOpen={isPhotoBookModalOpen}
-        setIsPhotoBookModalOpen={setIsPhotoBookModalOpen}
-        photoBookOptions={photoBookOptions}
-        onCreatePhotoBook={handleCreatePhotoBook}
-      />
-    </div>
+    <>
+      {!isLoading && isAuthenticated && (
+        <div>
+          <PhotoBookModal
+            isOpen={isPhotoBookModalOpen}
+            onClose={() => setIsPhotoBookModalOpen(false)}
+            onCreate={handleCreatePhotoBook}
+            options={photoBookOptions}
+            onChange={handlePhotoBookChange}
+          />
+          <ImageGallery
+            onDrop={handleDrop}
+            images={images}
+            isVisible={isGalleryVisible}
+          />
+          <ImageCanvas
+            images={images}
+            isGalleryVisible={isGalleryVisible}
+            setIsGalleryVisible={setIsGalleryVisible}
+            isPhotoBookModalOpen={isPhotoBookModalOpen}
+            setIsPhotoBookModalOpen={setIsPhotoBookModalOpen}
+            photoBookOptions={photoBookOptions}
+            onCreatePhotoBook={handleCreatePhotoBook}
+          />
+        </div>
+      )}
+    </>
   );
 };
 
